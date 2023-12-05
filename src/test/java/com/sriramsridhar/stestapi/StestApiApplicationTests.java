fd7e17ffb01711544a5bd2a566d0c579482e1470
sriram sridhar <svss1995@gmail.com>
2023-12-06 01:03:51 +0530

    fixes

--- src/test/java/com/sriramsridhar/stestapi/StestApiApplicationTests.java
+++ src/test/java/com/sriramsridhar/stestapi/StestApiApplicationTests.java
@@ -3,11 +3,14 @@ package com.sriramsridhar.stestapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class StestApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
